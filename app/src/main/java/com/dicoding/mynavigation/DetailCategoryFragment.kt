57f7fb01664ec3016301a4741e92e3f03e63f1aa
Yuandika Alfahreiza <yuandika11@gmail.com>
2021-12-07 21:15:45 +0700

    add: pop up behaviour

--- app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/dicoding/mynavigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.dicoding.mynavigation.databinding.FragmentDetailCategoryBinding
 
 /**
@@ -38,6 +39,9 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock = $dataDescription"
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
